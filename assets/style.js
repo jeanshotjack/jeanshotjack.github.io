c87b6c6d2db6b39ded22a0af6cdd080e010be5b8
jeanshotjack <dshalenkova@gmail.com>
2019-11-08 00:36:22 -0500

    updates

--- assets/style.js
+++ assets/style.js
@@ -3,17 +3,11 @@ $(document).ready(function() {
     $("#resume").hide();
     $("#abt").on("click", function() {
         $("#gallery").hide();
-        $("#resume").hide();
         $("#about").show();
     });
     $("#gal").on("click", function() {
         $("#about").hide();
-        $("#resume").hide();
         $("#gallery").show();
     });
-    $("#res").on("click", function() {
-        $("#about").hide();
-        $("#gallery").hide();
-        $("#resume").show();
-    });
+
 });
